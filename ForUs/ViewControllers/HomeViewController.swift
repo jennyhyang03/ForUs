e1ff652b73702deba6a111df5880a7d4e8c2179e
Jia Michel <michelj9012@gmail.com>
2022-08-03 19:14:48 +0200

    * Added firebase stuff- yet to work * Log in and Username button * Beginning of News API

---
+++ ForUs/ViewControllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  ForUs
+//
+//  Created by J M on 8/2/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "News"
+    
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ForUs/ViewControllers/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ForUs
+//
+//  Created by J M on 8/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
